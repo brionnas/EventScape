a6e7134cfc88efe437de6dcbf73087e1f9102a92
brionnas <brionnaswinton@gmail.com>
2025-06-02 13:39:35 -0400

    added packages

---
+++ eventscape/src/main/java/com/model/DataLoader.java
@@ -0,0 +1,12 @@
+package com.model;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
+
+public class DataLoader {
+    
+}
--- eventscape/src/main/java/module-info.java
+++ eventscape/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.event {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.event to javafx.fxml;
     exports com.event;
