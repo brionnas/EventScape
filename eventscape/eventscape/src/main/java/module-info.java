a646d75d016e129e99ef19e176265a4027ff066a
chisholmfps <jchish9900@gmail.com>
2025-06-02 13:58:08 -0400

    deleted the 2 extra pom.xml and App.javas

--- eventscape/eventscape/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.event {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.event to javafx.fxml;
-    exports com.event;
-
-    opens com.controllers to javafx.fxml;
-}
