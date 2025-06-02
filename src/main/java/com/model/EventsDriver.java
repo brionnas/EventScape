aaf77bbf5ef67c783b9a9586da6984df9777ca1c
chisholmfps <jchish9900@gmail.com>
2025-06-02 13:37:12 -0400

    deleted dupe files

--- src/main/java/com/model/EventsDriver.java
+++
@@ -1,7 +0,0 @@
-package com.model;
-
-public class EventsDriver {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
--- src/main/java/json/UUIDmain.java
+++
@@ -1,8 +0,0 @@
-package json;
-import java.util.UUID;
-
-public class UUIDmain {
-    public static void main(String[] args) {
-        System.out.println(UUID.randomUUID());
-    }
-}
