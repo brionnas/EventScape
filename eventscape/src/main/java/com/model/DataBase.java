1e98c09b37d3ed84fdea92606b446356121d2ab8
omatt10 <mattheo@email.sc.edu>
2025-06-03 13:49:00 -0400

    working on #53

---
+++ eventscape/src/main/java/com/model/DataBase.java
@@ -0,0 +1,15 @@
+package com.model;
+
+
+import java.util.ArrayList;
+
+public class DataBase {
+
+    public static ArrayList<User> getUsers(){
+		return new ArrayList<User>();
+	}
+	
+	public static ArrayList<User> getItems(){
+		return new ArrayList<User>();
+	}
+}
