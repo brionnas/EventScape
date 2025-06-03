9a52dd003e3b2b01c17851bd3dadb8733d834e14
omatt10 <mattheo@email.sc.edu>
2025-06-02 20:50:19 -0400

    data constants

---
+++ src/main/java/com/model/DataConstants.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class DataConstants {
+    protected static final String USER_FILE_NAME = "src/main/java/com/data/users.json"; 
+    protected static final String USER_TEMP_FILE_NAME = "src/main/java/com/data/users_temp";
+    protected static final String EVENT_ID = "id";
+    protected static final String USER_FIRST_NAME = "userName";
+    protected static final String USER_LAST_NAME = "LastName"; 
+    protected static final String USER_AGE = "age";
+    protected static final String USER_PHONE_NUMBER = "phoneNumber"; 
+
+
+}
