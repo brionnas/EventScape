df14f5364779e24dd22a8bcadfab63e4ade681ca
CSE-ICollins <ICOLLINS@email.sc.edu>
2025-06-03 20:37:30 -0400

    fixed mistakes from classes not done

--- eventscape/src/main/java/com/model/EventsDriver.java
+++ eventscape/src/main/java/com/model/EventsDriver.java
@@ -1,7 +1,12 @@
 package com.model;
 
 public class EventsDriver {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        String userFilePath = "eventscape/src/main/java/com/model/User.java"; // Adjust to your project structure
+        List<User> users = DataLoader.loadUsers(userFilePath);
+
+        for (User user : users) {
+            System.out.println("User: " + user.getUserName() + " | Email: " + user.getEmail());
+        }
     }
 }
--- eventscape/src/main/java/com/model/User.java
+++ eventscape/src/main/java/com/model/User.java
@@ -1,7 +1,8 @@
 package com.model;
+
 import java.util.*;
 
-public class User{
+public class User {
     private String userName;
     private String firstName;
     private String lastName;
@@ -12,72 +13,50 @@ public class User{
     private boolean isLocked;
     private int failedLoginAttempts;
     private boolean studentVerified;
-    //private List<Ticket> tickets;
-
+    private List<Ticket> tickets;
+ 
     public User(String userName, String firstName, String lastName,
-    String email, String phoneNumber, String passwordHash){
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.birthDate = birthDate;
-        this.passwordHash = passwordHash;
-        this.isLocked = false;
-        //this.failedLoginAttempts = false;
-        this.studentVerified = false;
-       // this.tickets = new ArrayList<>();
-    }
-
-    public User(UUID id, String userFirstName, String userLastName, int age, String phoneNumber2) {
-        //TODO Auto-generated constructor stub
-    }
-
-    public String getUserName(){
-        return userName;
-    }
-
-    public String getFirstName(){
-        return firstName;
-    }
-
-    public String getLastName(){
-        return lastName;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getPhoneNumber(){
-        return phoneNumber;
-    }
-
-    public String getBirthDate(){
-        return birthDate;
-    }
-
-    public String getPasswordHash(){
-        return passwordHash;
-    }
-
-    public boolean getIsLocked(){
-        return isLocked;
-    }
-
-    public int getFailedLoginAttempts(){
-        return failedLoginAttempts;
-    }
-
-    public boolean getStudentVerified(){
-        return false;
-    }
-/* 
-    public List<Tickets> getTickets(){
-        return tickets;
-    }
-*/
-    public String toString() {
-        return firstName + " " + lastName;
-    }
+            String email, String phoneNumber, String birthDate,
+            String passwordHash, boolean isLocked,
+            int failedLoginAttempts, boolean studentVerified) {
+    this.userName = userName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.birthDate = birthDate;
+    this.passwordHash = passwordHash;
+    this.isLocked = isLocked;
+    this.failedLoginAttempts = failedLoginAttempts;
+    this.studentVerified = studentVerified;
+    this.tickets = new ArrayList<>();
 }
+
+    public String getUserName() { 
+        return userName; }
+    public String getFirstName() { 
+        return firstName; }
+    public String getLastName() { 
+        return lastName; }
+    public String getEmail() { 
+        return email; }
+    public String getPhoneNumber() { 
+        return phoneNumber; }
+    public String getBirthDate() { 
+        return birthDate; }
+    public String getPasswordHash() {
+        return passwordHash; }
+    public boolean getIsLocked() { 
+        return isLocked; }
+    public int getFailedLoginAttempts() { 
+        return failedLoginAttempts; }
+    public boolean getStudentVerified() { 
+        return studentVerified; }
+    public List<Ticket> getTickets() { 
+        return tickets; }
+         public void setTickets(List<Ticket> tickets) { this.tickets = tickets; }
+    @Override
+public String toString() {
+    return userName + ": " + firstName + " " + lastName + ", Email: " + email;
+}
+
