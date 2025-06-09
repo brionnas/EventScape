ba810a1d5ae6630bfc7ef747d4ed7f0e48d64e1d
omatt10 <mattheo@email.sc.edu>
2025-06-09 13:17:25 -0400

    working test case

--- eventscape/src/test/java/com/model/FacadeTests.java
+++ eventscape/src/test/java/com/model/FacadeTests.java
@@ -1,10 +1,12 @@
+package com.model; 
+
 import static org.junit.Assert.assertEquals; 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue; 
 
 import org.junit.Test; 
 
-import model.Facade; 
+import com.model.Facade; 
 
 public class FacadeTests {
 
