0c7cf71ecbb1b72f52afc3fd076b6f728f064578
CSE-ICollins <ICOLLINS@email.sc.edu>
2025-06-10 12:42:33 -0400

    tester for facade

--- eventscape/src/test/java/com/model/FacadeTests.java
+++ eventscape/src/test/java/com/model/FacadeTests.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue; 
 
 import org.junit.Test; 
+import com.model.Facade; 
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
