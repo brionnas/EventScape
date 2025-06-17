c304bc432affc413371887ba067c5cc58ccd43ff
brionnas <brionnaswinton@gmail.com>
2025-06-17 12:09:20 -0400

    transfered to home page

--- eventscape/src/main/java/com/controllers/Home.java
+++ eventscape/src/main/java/com/controllers/Home.java
@@ -1,17 +1,49 @@
 package com.controllers;
 
+import com.model.Facade;
+import com.model.User;
+import com.model.Event;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class Home {
 
     @FXML
-    private Button HomeButton;
+    private Label welcomeLabel;
 
     @FXML
-    void switchToPrimary(ActionEvent event) {
+    private VBox eventListVBox;
 
+    @FXML
+    private Button secondaryButton;
+
+    private User currentUser;
+
+    public void setUser(User user) {
+        this.currentUser = user;
+        welcomeLabel.setText("Welcome, " + user.getFirstName() + "!");
+
+        // Load and display events
+        Facade facade = Facade.getInstance();
+        List<Event> events = facade.getAllEvents(); // make sure this method exists
+
+        for (Event e : events) {
+            Label eventLabel = new Label(e.getName() + " - " + e.getDate());
+            eventListVBox.getChildren().add(eventLabel);
+        }
     }
 
+    @FXML
+    private void switchToPrimary(ActionEvent event) {
+        System.out.println("Back to primary view logic goes here...");
+        // Add logic to switch to login or another scene if needed
+    }
 }
+
+
--- eventscape/src/main/java/com/controllers/LoginController.java
+++ eventscape/src/main/java/com/controllers/LoginController.java
@@ -1,13 +1,20 @@
 package com.controllers;
 
+import java.io.IOException;
+
 import com.model.Facade;
 import com.model.User;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class LoginController {
 
@@ -24,19 +31,34 @@ public class LoginController {
     private TextField txtUserName;
 
     @FXML
-    void login(ActionEvent event) {
-         String userName = txtUserName.getText();
-        String password = txtPassword.getText(); 
 
-        Facade facade = Facade.getInstance(); 
+void login(ActionEvent event) {
+    String userName = txtUserName.getText();
+    String password = txtPassword.getText(); 
 
-        User user = facade.login(userName, password);
-        if( user == null) {
-            System.out.println("error");
-            return;
-        }
-        System.out.println("Welcome " + user.getFirstName());
+    Facade facade = Facade.getInstance(); 
+    User user = facade.login(userName, password);
 
+    if (user == null) {
+        lblErrorMessage.setText("Invalid username or password.");
+        return;
     }
 
+    System.out.println("Welcome " + user.getFirstName());
+
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/event/home.fxml"));
+        Parent root = loader.load();
+
+        // Access Home controller and pass the User
+        Home homeController = loader.getController();
+        homeController.setUser(user);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
 }
